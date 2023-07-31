e688640d4f43152eb625974faa341419c701648b
unknown <s200236@mail.gpwu.ac.jpgit config --global user.name yuka-cyan>
2023-07-31 12:05:42 -0700

    [update] WEBアプリケーションを作ってみようー２

--- omikuji.js
+++ omikuji.js
@@ -5,7 +5,12 @@ let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
 
-let rand = Math.floor( Math.random() * 5);
+if (username == "") {
+  username = "名無し";
+}
+document.getElementById("name").innerHTML = username;
+
+led = Math.floor( Math.random() * 5);
 if (rand == 0) {
   userresult = "大吉";
 }
